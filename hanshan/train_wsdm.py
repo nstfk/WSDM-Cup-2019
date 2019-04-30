ad42a29ad3ece1da49933b4d0e46c626c87922c8
nstfk <nohaseddik@hotmail.com>
2019-04-30 02:29:19 +0200

    Update train_wsdm.py

--- hanshan/train_wsdm.py
+++ hanshan/train_wsdm.py
@@ -393,7 +393,7 @@ def main():
             tr_loss, train_accuracy = 0, 0
             nb_tr_examples, nb_tr_steps = 0, 0
             for step, batch in enumerate(tqdm(train_dataloader,
-                                              desc="Iteration")):
+                                              desc="Iteration",mininterval=60)):
                 batch = tuple(t.to(device) for t in batch)
                 input_ids, input_mask, segment_ids, label_ids = batch
                 #print(input_ids.shape, input_mask.shape, segment_ids.shape, label_ids.shape)
